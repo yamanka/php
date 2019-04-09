96f0ccf278a616e2b5fe4831415a250bd744712c
yamanka <yamanka@outlook.com>
2019-04-09 11:34:38 +0800

    modify

--- punch.go
+++ punch.go
@@ -61,7 +61,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// 设置路由，如果访问/，则调用index方法
-	http.HandleFunc("/", index)
+	http.HandleFunc("/punch", index)
 
 	// 启动web服务，监听9090端口
 	err := http.ListenAndServe(":9090", nil)
